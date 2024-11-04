881939e9241229ccfad88ec6a4a5545d9580a30f
Your Name <you@example.com>
2024-11-04 18:34:57 +0800

    initial commit

---
+++ classSubject.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Data Structure and Algorithms";
+let classSchedule = "9:30 - 10:30 in Monday and Friday";
+let Classroom = "Computer Laboratory A";
+let classInstructor = "Mr.Vitruvius John Barayuga";
+let StudentName = " Denver G. Valentino";
+
+console.log(`${StudentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${Classroom}.The instructor for the subject is ${classInstructor}.`);
